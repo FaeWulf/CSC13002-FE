c039861e07b2ecb397a0c1ffe83c0a3281e72511
20120298 <pbhuy0909@gmail.com>
2022-12-22 17:57:35 +0700

    add axios module

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
-import { createApp } from 'vue';
+import * as Vue from 'vue'; // in Vue 3
 import App from './App.vue';
 import router from '../src/routers/index';
-import './assets/css/style.css'
+import './assets/css/style.css';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 
-const app = createApp(App);
+const app = Vue.createApp(App);
 app.config.globalProperties.gUserName = '';
 app.config.globalProperties.gUserType = -1;
 app.use(router);
+app.use(VueAxios, axios);
 app.mount('#app');
 
 export { app };
