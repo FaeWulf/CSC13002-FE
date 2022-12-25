25c0b9c28be346a6d4ebac676cbd2a9c1b1a5ba0
20120298 <pbhuy0909@gmail.com>
2022-12-25 22:43:53 +0700

    final project for xmas

--- src/main.js
+++ src/main.js
@@ -2,12 +2,11 @@ import * as Vue from 'vue';
 import App from './App.vue';
 import router from '../src/routers/index';
 import './assets/css/style.css';
-// const bcrypt = require('bcrypt');
 
 const app = Vue.createApp(App);
 app.config.globalProperties.gUserName = '';
 app.config.globalProperties.gUserType = -1;
-app.config.globalProperties.base_url = 'http://api.faewulf.xyz:3000';
+app.config.globalProperties.base_url = 'http://localhost:3000';
 app.use(router);
 app.mount('#app');
 
